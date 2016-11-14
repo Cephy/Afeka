216c2259076f92173a6ed787745f404211b2bc05
Cephy <sefi24@gmail.com>
2016-11-14 22:23:44 +0200

    123

--- sefi/src/practice_makes_perfect/presentation3/Ch1_q2.java
+++ sefi/src/practice_makes_perfect/presentation3/Ch1_q2.java
@@ -1,10 +1,12 @@
 package practice_makes_perfect.presentation3;
 
+import java.util.*;
+
 public class Ch1_q2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	
 	}
 
 }
