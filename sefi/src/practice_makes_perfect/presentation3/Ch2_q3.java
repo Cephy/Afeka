451671114c0874c5420dd13c433b73c561cc7dd0
Cephy <sefi24@gmail.com>
2016-11-20 00:03:33 +0200

    update

--- sefi/src/practice_makes_perfect/presentation3/Ch2_q3.java
+++ sefi/src/practice_makes_perfect/presentation3/Ch2_q3.java
@@ -7,16 +7,24 @@ public class Ch2_q3 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		final int DIGIT_NUM_FACTOR = 2;
-		int num, DigitNumber, num1 = 0;
+		int num, DigitNumber=0, num1 = 0, num2 = 2;
 		
 		do {
-			System.out.print("please entre a number");
+			System.out.println("please entre a number");
 			num = s.nextInt();
-			for (int i=num ; i!=0 ; i/10 )
+			DigitNumber = 0;
+			for (int i=num ; i!=0 ; i/=10){
 				DigitNumber++;
-				num1 = i%10
-		}while ()
-
+				if (DigitNumber==1) {
+					num1 = i%10;
+				}
+				if (DigitNumber==2){
+					num2 = i%10;
+				}
+			}
+			System.out.printf("your number is %d \n", num);
+		}while (!(DigitNumber==2 && num2==num1));
+		System.out.print("thank you");
 	}
 
 }
