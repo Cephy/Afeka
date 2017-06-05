d5fc5b0845e3c8f1d48ae047f6221df75b36e60f
Cephy <sefi24@gmail.com>
2017-06-05 21:05:49 +0300

    update

--- sefi/src/Dangerous/Dangerous.java
+++
@@ -1,8 +0,0 @@
-package Dangerous;
-
-import java.util.GregorianCalendar;
-
-public interface Dangerous {
-		boolean isDangerous();
-		int YEAR = new GregorianCalendar().get(GregorianCalendar.YEAR);
-}
