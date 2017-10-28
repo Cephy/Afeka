7da2eedf1a2c3a85ed04b76a2aa674f568129302
Cephy <sefi24@gmail.com>
2017-10-28 12:42:51 +0300

    update

---
+++ sefi/src/simester1B/practice1/Q2.java
@@ -0,0 +1,16 @@
+package simester1B.practice1;
+
+import java.io.File;
+
+public class Q2 {
+	
+	public static void main (String[] args){
+		int[] a = new int[]{10,20,30,40,50,60};
+		File file = new file ("1");
+			
+		
+		
+	}
+	
+	
+}
